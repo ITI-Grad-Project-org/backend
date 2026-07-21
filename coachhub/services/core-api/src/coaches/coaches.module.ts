492a164cd157dc54aea027f7ec818a2bad7b89da
omarkobro <omakobro123@gmail.com>
2026-07-21 03:07:01 +0300

    feat(exercises): seed defaults and initialize tenant libraries

--- coachhub/services/core-api/src/coaches/coaches.module.ts
+++ coachhub/services/core-api/src/coaches/coaches.module.ts
@@ -4,11 +4,12 @@ import { CoachesService } from './coaches.service';
 import { CoachesController } from './coaches.controller';
 import { Coach } from './entities/coach.entity';
 import { TenantModule } from '../tenant/tenant.module';
+import { ExercisesModule } from '../exercises/exercises.module';
 
 @Module({
 	controllers: [CoachesController],
 	providers: [CoachesService],
-	imports: [TypeOrmModule.forFeature([Coach]), TenantModule],
+	imports: [TypeOrmModule.forFeature([Coach]), TenantModule, ExercisesModule],
 	exports: [CoachesService],
 })
 export class CoachesModule {}
